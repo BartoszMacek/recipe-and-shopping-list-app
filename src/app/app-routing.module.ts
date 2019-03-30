3fbe8cfd776607bbd8c431ea8c4958072b273804
Bartosz Macek <bartoszmacek@gmail.com>
2019-03-30 15:20:40 +0000

    Recipe and shopping list // Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'shopping-list', component: ShoppingListComponent},
+  {path: 'recipes', component: RecipesComponent, children: [
+      {path: '', component: RecipeStartComponent, data: {message: 'Please select recipe!'}},
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id', component: RecipeDetailComponent},
+      {path: ':id/edit', component: RecipeEditComponent}
+    ]}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
